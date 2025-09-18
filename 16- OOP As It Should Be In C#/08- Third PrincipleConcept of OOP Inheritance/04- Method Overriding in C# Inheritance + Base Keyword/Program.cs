1adfff17d43f4da9c4e5099c3ac73e87b8ac71ce
Simoxii48 <mohahejjam@gmail.com>
2025-09-18 15:29:29 +0200

    Method Override & Base Keyword Usecases within inheritance classes

---
+++ 16- OOP As It Should Be In C#/08- Third PrincipleConcept of OOP Inheritance/04- Method Overriding in C# Inheritance + Base Keyword/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public  class clsA
+{
+
+    // Virtual keyword allows a method to be overridden in a derived class
+    public virtual void  Print()
+    {
+        Console.WriteLine("Hi, I'm the print method from the base class A");
+    }
+
+}
+
+public class clsB : clsA
+{
+
+    // Override keyword is used to extend or modify a virtual/abstract method in a derived class
+    public override void  Print() 
+    
+    {
+        Console.WriteLine("Hi, I'm the print method from the derived class B");
+        // base keyword is used to access members of the base class from within a derived class
+        base.Print();       
+      
+     }
+}
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+       
+        //Create an object of Empoyee
+        clsB ObjB= new clsB();
+        ObjB.Print();
+        Console.ReadKey();
+        }
+    }
+
