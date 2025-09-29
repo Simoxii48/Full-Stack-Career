2611a642f913dc9f7c4d73b11d10b4a9d039f524
Simoxii48 <mohahejjam@gmail.com>
2025-09-29 16:43:10 +0200

    Distinct keyword used to avoid duplication and double inner joins

---
+++ 17 - Database - SQL ( Projects & Practice)/04- SQL Problems/12- Problem 10 Get all Makes that runs with GAS/distinct and double inner joins.sql
@@ -0,0 +1,6 @@
+
+select distinct Makes.Make, FuelTypes.FuelTypeName
+from VehicleDetails 
+				inner join FuelTypes on VehicleDetails.FuelTypeID = FuelTypes.FuelTypeID
+				inner join Makes on VehicleDetails.MakeID = Makes.MakeID
+where FuelTypes.FuelTypeName = N'GAS'
