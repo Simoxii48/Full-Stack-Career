f7b2e7595256ff20cfd90b98783101b006638bb7
Simoxii48 <mohahejjam@gmail.com>
2025-09-29 14:30:35 +0200

    Select count(*) statement used inside the select statement to solve the problem, select statement can be dealed with as a column result

---
+++ 17 - Database - SQL ( Projects & Practice)/04- SQL Problems/08-  Problem 6 Get number of vehicles made between 1950 and 2000 per make and add total vehicles column beside/select inside select statement.sql
@@ -0,0 +1,7 @@
+
+
+select Makes.Make as Make, count(*) as NumberOfVehicules, (select count(*) from VehicleDetails) as TotalVehicules
+from VehicleDetails inner join Makes on VehicleDetails.MakeID = Makes.MakeID
+where VehicleDetails.Year between 1950 and 2000
+group by Makes.Make
+order by NumberOfVehicules desc
