13264a323a71f49cf69b2b9a258681b29e73a4b1
Simoxii48 <mohahejjam@gmail.com>
2025-09-30 11:54:29 +0200

    Distinct used to preserve duplicated data & double inner joins as well to get data across tables

---
+++ 17 - Database - SQL ( Projects & Practice)/04- SQL Problems/18- Problem 16 Get all Makes that manufactures DriveTypeName = FWD/Distinct & double joins.sql
@@ -0,0 +1,6 @@
+
+select distinct Makes.Make,DriveTypes.DriveTypeName
+from VehicleDetails
+		inner join Makes on VehicleDetails.MakeID = Makes.MakeID
+		inner join DriveTypes on VehicleDetails.DriveTypeID = DriveTypes.DriveTypeID
+where DriveTypes.DriveTypeName = 'FWD'
