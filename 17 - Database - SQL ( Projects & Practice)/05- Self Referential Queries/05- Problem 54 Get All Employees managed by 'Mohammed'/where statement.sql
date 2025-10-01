6e4ce7b26038148977a2a5b7822cc2a90244ce59
Simoxii48 <mohahejjam@gmail.com>
2025-10-01 15:54:10 +0200

    Self Ref Inner Join with where filter added

---
+++ 17 - Database - SQL ( Projects & Practice)/05- Self Referential Queries/05- Problem 54 Get All Employees managed by 'Mohammed'/where statement.sql
@@ -0,0 +1,5 @@
+
+SELECT Employees.Name, Employees.ManagerID, Employees.Salary, Managers.Name AS ManagerName
+FROM   Employees INNER JOIN
+       Employees AS Managers ON Employees.ManagerID = Managers.EmployeeID
+where Managers.Name='Mohammed'
