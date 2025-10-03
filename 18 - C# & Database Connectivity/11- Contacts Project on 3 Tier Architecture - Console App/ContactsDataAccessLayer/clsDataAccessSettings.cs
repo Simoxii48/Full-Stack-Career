fa1a6ccdcfb9bd24ef251754f6e65128f87685fd
Simoxii48 <mohahejjam@gmail.com>
2025-10-03 18:21:08 +0200

    Separated Class hold the string connection that we use mostly for not repeating ourselves

---
+++ 18 - C# & Database Connectivity/11- Contacts Project on 3 Tier Architecture - Console App/ContactsDataAccessLayer/clsDataAccessSettings.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ContactsDataAccessLayer
+{
+    static class ClsDataAccessSettings
+    {
+        static public string connectionString = "server=.;Database=ContactsDB;user id=sa;password=sa123456;";
+    }
+}
