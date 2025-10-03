3b39e42f70bf815069a06554639a940262ecf672
Simoxii48 <mohahejjam@gmail.com>
2025-10-03 18:24:47 +0200

    Console App Data Access Layer Class Created to talk with our Database and retrieve the data to the Business Layer the from it To the Presentation Layer creating here The 3 Tier App Architecture & Separation of concern, each layer working independently than the other, all the Methods are static, And the SQLClient is used only here in this layer

---
+++ 18 - C# & Database Connectivity/11- Contacts Project on 3 Tier Architecture - Console App/ContactsDataAccessLayer/ContactsDataAccess.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ContactsDataAccessLayer
+{
+    public class ClsContactsDataAccess
+    {
+        public static bool GetContactInfoByID(int ContactID, ref string Firstname, ref string Lastname, ref string ContactEmail, ref string ContactPhone,
+            ref string ContactAddress, ref DateTime DateOfBirth, ref int CountryID, ref string ImagePath)
+        {
+            bool isFound = false;
+            SqlConnection connection = new SqlConnection(ClsDataAccessSettings.connectionString);
+            string query = "Select * from Contacts where ContactID=@ContactID";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@ContactID", ContactID);
+
+            try // will execute only if no exception occurs
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if(reader.Read()) // If a record is found
+                {
+                    Firstname = reader["Firstname"].ToString();
+                    Lastname = reader["Lastname"].ToString();
+                    ContactEmail = reader["Email"].ToString();
+                    ContactPhone = reader["Phone"].ToString();
+                    ContactAddress = reader["Address"].ToString();
+                    DateOfBirth = (DateTime)reader["DateOfBirth"];
+                    CountryID = (int) reader["CountryID"];
+                    ImagePath = (string) reader["ImagePath"];
+                    
+                    isFound = true;
+                }
+                else
+                    isFound = false; // No record found
+
+                reader.Close();
+            }
+            catch (Exception ex) // will execute only if an exception occurs
+            {
+                throw new Exception("Error in GetContactInfoByID: " + ex.Message);
+            }
+            finally // will execute always
+            {
+                connection.Close(); // best practice to close the connection in the finally block if error occurs or not ensures connection is closed
+            }
+
+            return isFound;
+        }
+    }
+}
