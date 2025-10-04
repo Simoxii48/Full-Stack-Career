a260df60b3ed44b6fa56a35a71347602c7271b22
Simoxii48 <mohahejjam@gmail.com>
2025-10-04 16:33:35 +0200

    Countries Data Access Layer Class Created to interact with the Database, FindCountryById Method was Created to return a boolean value if a country with a given Id was found or not using the try catch finally blocks as usual and the ExecuteReader() Method

---
+++ 18 - C# & Database Connectivity/11- Contacts Project on 3 Tier Architecture - Console App/CountriesDataAccessLayer/CountriesDataAccess.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CountriesDataAccessLayer
+{
+    public class clsCountriesDataAccess
+    {
+        public static bool FindCountryByID(int CountryId, ref string countryName)
+        {
+            bool isCountryFound = false;
+            SqlConnection connection = new SqlConnection(clsCountriesDataAccessSettings.connectionString);
+            string query = "SELECT * FROM Countries WHERE CountryID = @CountryID";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@CountryID", CountryId);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    // Assuming CountryName is the second column in the Countries table
+                    countryName = reader["CountryName"].ToString();
+                    isCountryFound = true;
+                }
+                else
+                {
+                    isCountryFound = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error finding country by ID: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return isCountryFound;
+        }
+    }
+}
