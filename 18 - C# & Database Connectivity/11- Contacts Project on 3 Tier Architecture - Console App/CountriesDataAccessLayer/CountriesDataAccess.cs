b35a9ebbcb0a866ce122655a3dff67573cf4bb64
Simoxii48 <mohahejjam@gmail.com>
2025-10-05 01:06:28 +0200

    Data Access Layer Updated with a static Method add new country, to insert the data to the database returning the last ID Inserted using the ExecuteScalar() method & select scope_identity() stored procedure in the DB, using the try catch finally blocks to handle exceptions

--- 18 - C# & Database Connectivity/11- Contacts Project on 3 Tier Architecture - Console App/CountriesDataAccessLayer/CountriesDataAccess.cs
+++ 18 - C# & Database Connectivity/11- Contacts Project on 3 Tier Architecture - Console App/CountriesDataAccessLayer/CountriesDataAccess.cs
@@ -28,6 +28,8 @@ namespace CountriesDataAccessLayer
                 {
                     isCountryFound = false;
                 }
+
+                reader.Close();
             }
             catch (Exception ex)
             {
@@ -39,6 +41,42 @@ namespace CountriesDataAccessLayer
             }
 
             return isCountryFound;
+        }
+
+        public static int AddNewCountry(string countryName)
+        {
+            SqlConnection connection = new SqlConnection(clsCountriesDataAccessSettings.connectionString);
+            string query = "insert into Countries (CountryName) values (@CountryName);" +
+                "Select Scope_Identity();";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@CountryName", countryName);
+            int newCountryID = -1;
+
+            try
+            {
+                connection.Open();
+                object result = command.ExecuteScalar();
+                
+                if(result != null && int.TryParse(result.ToString(), out int CountryID))
+                {
+                    // Successfully retrieved the new CountryID
+                    newCountryID = CountryID;
+                }
+                else
+                {
+                    throw new Exception("Failed to retrieve the new CountryID.");
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Error adding new country: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return newCountryID;
         }
     }
 }
