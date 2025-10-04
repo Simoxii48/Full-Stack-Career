6471c6a7860bdc3547e4f5d166675c8b78dfd6cf
Simoxii48 <mohahejjam@gmail.com>
2025-10-05 01:03:46 +0200

    Business Layer Updated with a private enum Mode to track if we are adding or updating, private & default constructor updated with the Mode, addNewCountry private Method added to interact with the Data Access Layer, finally the Save method updated to handle the add new mode case

--- 18 - C# & Database Connectivity/11- Contacts Project on 3 Tier Architecture - Console App/CountriesBusinessLayer/Countries.cs
+++ 18 - C# & Database Connectivity/11- Contacts Project on 3 Tier Architecture - Console App/CountriesBusinessLayer/Countries.cs
@@ -6,15 +6,30 @@ namespace CountriesBusinessLayer
 {
     public class clsCountries
     {
+        public enum enMode { AddNew, Update };
+
+        private enMode Mode; // To track whether we are adding a new country or updating an existing one
+
         // Properties
         public int CountryID { get; set; }
         public string CountryName { get; set; }
 
+        // Private constructor to enforce the use of the Find method
         private clsCountries(int countryID, string countryName)
         {
             CountryID = countryID;
             CountryName = countryName;
+
+            Mode = enMode.Update;
         }
+
+        // Default constructor
+        public clsCountries()
+        {
+            CountryName = "";
+            Mode = enMode.AddNew;
+        }
+
         // Find a country by ID
         public static clsCountries Find(int CountryID)
         {
@@ -23,6 +38,34 @@ namespace CountriesBusinessLayer
                 return new clsCountries(CountryID, countryName);
             else
                 return null;
+        }
+
+        // Private method to add a new country
+        private bool _AddNewCountry()
+        {
+            // Call the data access layer to add a new country
+            this.CountryID = clsCountriesDataAccess.AddNewCountry(this.CountryName);
+            return this.CountryID != -1; // Return true if CountryID is assigned
+        }
+
+        // Save method
+        public bool Save()
+        {
+            switch(Mode)
+            {
+                case enMode.AddNew:
+                    if(_AddNewCountry())
+                    {
+                        Mode = enMode.Update; // Switch to update mode after adding
+                        return true;
+                    }
+                    else
+                        return false;
+                case enMode.Update:
+
+                default:
+                    return false;
+            }
         }
     }
 }
