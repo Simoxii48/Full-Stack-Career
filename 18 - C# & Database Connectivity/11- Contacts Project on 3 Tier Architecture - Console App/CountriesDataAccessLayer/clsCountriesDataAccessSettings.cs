485152d5bb2e65372e855486cce8d070b52b2006
Simoxii48 <mohahejjam@gmail.com>
2025-10-04 16:30:34 +0200

    Countries Data Access Settings Class Created with the connectionString We need for now

---
+++ 18 - C# & Database Connectivity/11- Contacts Project on 3 Tier Architecture - Console App/CountriesDataAccessLayer/clsCountriesDataAccessSettings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Data;
+
+namespace CountriesDataAccessLayer
+{
+    internal class clsCountriesDataAccessSettings
+    {
+        public static string connectionString = "server=.;database=ContactsDB;user id=sa;password=sa123456;";
+    }
+}
