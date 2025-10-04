4d2b267665de8d293d158eba9e5077303b766b73
Simoxii48 <mohahejjam@gmail.com>
2025-10-04 16:29:36 +0200

    Countries Business Layer Class Created with properties parameterized private construtor used internally with the find Method, and a find method that interact with the Countries Data Access Layer returning an object if country by an ID found and null if not referenced with the Last one of course (Reference done for the presentation layer and the business layer forget to mention)

---
+++ 18 - C# & Database Connectivity/11- Contacts Project on 3 Tier Architecture - Console App/CountriesBusinessLayer/Countries.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+using CountriesDataAccessLayer;
+
+namespace CountriesBusinessLayer
+{
+    public class clsCountries
+    {
+        // Properties
+        public int CountryID { get; set; }
+        public string CountryName { get; set; }
+
+        private clsCountries(int countryID, string countryName)
+        {
+            CountryID = countryID;
+            CountryName = countryName;
+        }
+        // Find a country by ID
+        public static clsCountries Find(int CountryID)
+        {
+            string countryName = "";
+            if(clsCountriesDataAccess.FindCountryByID(CountryID,ref countryName))   
+                return new clsCountries(CountryID, countryName);
+            else
+                return null;
+        }
+    }
+}
