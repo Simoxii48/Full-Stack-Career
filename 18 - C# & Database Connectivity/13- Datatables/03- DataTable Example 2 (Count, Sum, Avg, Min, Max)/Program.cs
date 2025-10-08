efd3c7f70b177a74e18be03245200155c1118aeb
Simoxii48 <mohahejjam@gmail.com>
2025-10-08 11:42:50 +0200

    Count, Sum, Avg, Min & Max aggregate functions withing the link library used with the DataTable

---
+++ 18 - C# & Database Connectivity/13- Datatables/03- DataTable Example 2 (Count, Sum, Avg, Min, Max)/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a DataTable to hold employee data
+            DataTable EmployeesDataTable = new DataTable();
+
+            EmployeesDataTable.Columns.Add("ID", typeof(int));
+            EmployeesDataTable.Columns.Add("Name", typeof(string));
+            EmployeesDataTable.Columns.Add("Country", typeof(string));
+            EmployeesDataTable.Columns.Add("Salary", typeof(double));
+            EmployeesDataTable.Columns.Add("Date", typeof(DateTime));
+
+
+            EmployeesDataTable.Rows.Add(1, "John Doe", "USA", 50000, DateTime.Now);
+            EmployeesDataTable.Rows.Add(2, "Jane Smith", "UK", 60000, DateTime.Now);
+            EmployeesDataTable.Rows.Add(3, "Sam Brown", "Canada", 55000, DateTime.Now);
+            EmployeesDataTable.Rows.Add(4, "Nancy White", "Australia", 70000, DateTime.Now);
+            EmployeesDataTable.Rows.Add(5, "Mike Green", "USA", 65000, DateTime.Now);
+
+            int EmployeesCount = EmployeesDataTable.Rows.Count;
+            double TotalSalaries = Convert.ToDouble(EmployeesDataTable.Compute("SUM(Salary)", string.Empty));
+            double AverageSalary = Convert.ToDouble(EmployeesDataTable.Compute("AVG(Salary)", string.Empty));
+            double MinSalary = Convert.ToDouble(EmployeesDataTable.Compute("MIN(Salary)", string.Empty));
+            double MaxSalary = Convert.ToDouble(EmployeesDataTable.Compute("MAX(Salary)", string.Empty));
+
+            Console.WriteLine("\nEmployees List :\n");
+
+            foreach (DataRow RecordRow in EmployeesDataTable.Rows)
+            {
+                Console.WriteLine("ID : {0} \t Name : {1} \t Country : {2} \t Salary : {3} \t Date : {4} \t",
+                    RecordRow["ID"], RecordRow["Name"], RecordRow["Country"], RecordRow["Salary"], RecordRow["Date"]);
+            }
+
+            Console.WriteLine("\nTotal Employees : {0}", EmployeesCount);
+            Console.WriteLine("Total Salaries : {0}", TotalSalaries);
+            Console.WriteLine("Average Salary : {0}", AverageSalary);
+            Console.WriteLine("Minimum Salary : {0}", MinSalary);
+            Console.WriteLine("Maximum Salary : {0}", MaxSalary);
+
+        }
+    }
+}
