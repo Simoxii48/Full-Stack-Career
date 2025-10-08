fc7d0b794503e6caf54143aec5f6721f01110758
Simoxii48 <mohahejjam@gmail.com>
2025-10-08 13:07:11 +0200

    Delete Rows Example

---
+++ 18 - C# & Database Connectivity/13- Datatables/06- Datatable Example 5 (Delete Rows)/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06__Datatable_Example_5__Delete_Rows_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a DataTable to hold employee data
+            DataTable EmployeesDataTable = new DataTable();
+
+            EmployeesDataTable.Columns.Add("ID", typeof(int));
+            EmployeesDataTable.Columns.Add("Name", typeof(string));
+            EmployeesDataTable.Columns.Add("Country", typeof(string));
+            EmployeesDataTable.Columns.Add("Salary", typeof(double));
+            EmployeesDataTable.Columns.Add("Date", typeof(DateTime));
+
+
+            EmployeesDataTable.Rows.Add(1, "John Doe", "USA", 50000, DateTime.Now);
+            EmployeesDataTable.Rows.Add(2, "Jane Smith", "UK", 60000, DateTime.Now);
+            EmployeesDataTable.Rows.Add(3, "Sam Brown", "Canada", 55000, DateTime.Now);
+            EmployeesDataTable.Rows.Add(4, "Nancy White", "Australia", 70000, DateTime.Now);
+            EmployeesDataTable.Rows.Add(5, "Mike Green", "USA", 65000, DateTime.Now);
+
+            Console.WriteLine("\nEmployees List :\n");
+
+            foreach (DataRow RecordRow in EmployeesDataTable.Rows)
+            {
+                Console.WriteLine("ID : {0} \t Name : {1} \t Country : {2} \t Salary : {3} \t Date : {4} \t",
+                    RecordRow["ID"], RecordRow["Name"], RecordRow["Country"], RecordRow["Salary"], RecordRow["Date"]);
+            }
+
+            // Delete Employee ID 4
+            Console.WriteLine("\nEmployees List After Deleting Employee ID 4 \n");
+
+            DataRow[] Results = EmployeesDataTable.Select("ID = 4");
+
+            foreach(var RecRow in Results)
+            {
+                RecRow.Delete();
+            }
+
+            foreach (DataRow RecordRow in EmployeesDataTable.Rows)
+            {
+                Console.WriteLine("ID : {0} \t Name : {1} \t Country : {2} \t Salary : {3} \t Date : {4} \t",
+                    RecordRow["ID"], RecordRow["Name"], RecordRow["Country"], RecordRow["Salary"], RecordRow["Date"]);
+            }
+
+        }
+    }
+}
