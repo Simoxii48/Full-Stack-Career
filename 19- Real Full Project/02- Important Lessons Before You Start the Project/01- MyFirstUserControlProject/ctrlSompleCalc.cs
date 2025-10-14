84110a4ce2a79f44c57a34c8a6afc716cc22e44d
Simoxii48 <mohahejjam@gmail.com>
2025-10-14 16:30:16 +0200

    Create a User Control to use in the whole system with its logic leaving us with a simple opperations & maintenance

---
+++ 19- Real Full Project/02- Important Lessons Before You Start the Project/01- MyFirstUserControlProject/ctrlSompleCalc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFirstUserControlProject
+{
+    public partial class ctrlSompleCalc : UserControl
+    {
+        public ctrlSompleCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            lblResults.Text = (int.Parse( textBox2.Text) + int.Parse(textBox2.Text)).ToString();
+
+        }
+    }
+}
